c9adf002636579e4954c8f78524d107f7bbe44bf
Marcel <marcel.basa@interia.pl>
2024-11-01 15:47:56 +0100

    improve css, remove all names drinks and add touchable area for random drinks card

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-let drinksNames = await getDrinks();
 let randomDrinks = await getRandomDrinks(4);
 
 app.get("/", async (req, res) => {
     res.render("index.ejs", {
-        drinks: drinksNames,
         random: randomDrinks
     });
 });
@@ -22,15 +20,13 @@ app.post("/submit", async (req, res) => {
     try{
         const result = await axios.get(`http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${req.body['drinkName']}`);
         res.render("index.ejs", {
-            drinks: drinksNames,
             random: randomDrinks,
             drinkDetails: result.data.drinks[0]
         });
     } catch (error) {
         res.render("index.ejs", {
-            drinks: drinksNames,
             random: randomDrinks,
-            error: "We don't have this coctail"
+            error: "We don't have this cocktail"
         });
     }
 });
@@ -52,14 +48,4 @@ async function getRandomDrinks(count){
         }
     }
     return drinkArr;
-}
-
-async function getDrinks(){
-    let result = [];
-    try{
-        result = await axios.get(`http://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic`);
-    } catch (error) {
-        console.log("Error during generate dinks log: "+error);
-    }
-    return result == [] ? result : result.data.drinks;
 }
